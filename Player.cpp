232c78dbcc303f0c5e1f2950671af06a4499d06a
nanoblob-jpeg <jerry.li1248@gmail.com>
2020-05-17 18:07:07 -0700

    commented some thoughts and changes to the Player.cpp file

--- Player.cpp
+++ Player.cpp
@@ -23,6 +23,26 @@ void Player::loadPlayer(const char *file){
 	//player doesn't start with any plants in the home page so there is no plant loading happening here
 	//player starts where the camera starts, so we don't need a position
 		//we can just set the position equal to the camera's position but in the 0 z axis
+	
+	//todo change this entire parsing part
+	//the stats of the player depend on the bowl that they are using and the buffs
+	//that are in effect at the time
+	//don't know if we should store the buffs active at the time in order to
+	//instantiate these or if we should just apply the buffs whenever they enter a new
+	//game, i'm leaning a bit more towards storing the buffs
+	//need an ending line then, cause they won't always have 9 buffs active
+	//boolean vector system for which buffs are active?
+	//should probably first think about all of the different buffs that a player can have
+	//
+	//from the plants in the green house
+	//achievements?
+	//they should level, so maybe this isn't bad, but maybe change all of this to be
+	//calculated off of their level
+	//i will just make the bowl add or subtract from their health/other stats when using them for
+	//calculations
+	//then with the greenhouse plants, instead of them increasing by percentage, have them increase
+	//by int, and then do the same thing as with the bowls and add or subtract them 
+	//when you need to do calculations with them
 	std::string line;
 	std::ifstream fstream(file);
 	if(fstream){
