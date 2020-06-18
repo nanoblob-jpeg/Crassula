4197002cdbee51b7db31965842bc30d1b695747a
nanoblob-jpeg <jerry.li1248@gmail.com>
2020-06-17 22:09:19 -0700

    i think i fixed texsamprenderer

--- TexSampRenderer.cpp
+++ TexSampRenderer.cpp
@@ -46,8 +46,7 @@ void TexSampRenderer::setOffset(glm::vec2 *data, int numOfOffsets){
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glVertexAttribDivisor(1, 6);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribDivisor(1, 1);
 	glBindVertexArray(0);
 }
 
@@ -58,8 +57,6 @@ void TexSampRenderer::setTextureCoords(glm::vec2 *data, int numOfCoords){
 
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glVertexAttribDivisor(2, 1);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
