ebe002388f94a8e2d3d36eb1757e3782437d18bc
nanoblob-jpeg <jerry.li1248@gmail.com>
2020-06-17 23:07:36 -0700

    pr

--- TexSampRenderer.cpp
+++ TexSampRenderer.cpp
@@ -58,7 +58,7 @@ void TexSampRenderer::setTextureCoords(glm::vec2 *data, int numOfCoords){
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glVertexAttribDivisor(2, 1);
+	glVertexAttribDivisor(2, 0);
 	glBindVertexArray(0);
 }
 
