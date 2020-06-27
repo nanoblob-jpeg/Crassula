785e0c6fec20751d063186f1c1326c3765563cb7
nanoblob-jpeg <jerry.li1248@gmail.com>
2020-06-27 14:25:24 -0700

    fs

--- Game.cpp
+++ Game.cpp
@@ -1554,7 +1554,7 @@ void Game::initShaders(){
 	ResourceManager::LoadShader("shaders/background_vshader.txt", "shaders/fragShader.txt", nullptr, "background_l3");
 	ResourceManager::LoadShader("shaders/text_vshader.txt", "shaders/fragShader.txt", nullptr, "text");
 	ResourceManager::LoadShader("shaders/vertexShader.txt", "shaders/fragShader.txt", nullptr, "UI");
-	ResourceManager::LoadShader("shader/texSamp_vshader.txt", "shader/fragShader_array.txt", nullptr, "icon");
+	ResourceManager::LoadShader("shaders/texSamp_vshader.txt", "shaders/fragShader_array.txt", nullptr, "icon");
 }
 
 void Game::initRenderers(){
