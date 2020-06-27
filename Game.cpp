63f09c07a9c9d4e2d3528278ce49bf8402896e11
nanoblob-jpeg <jerry.li1248@gmail.com>
2020-06-27 15:34:22 -0700

    fs

--- Game.cpp
+++ Game.cpp
@@ -1542,7 +1542,7 @@ void Game::calculateIconRenderValues(){
 	//top side is at 750
 	short starting_offset = player.bowl->numOfPlants == 3 ? 145 : 95;
 	for(int i{}; i < player.plants.size(); ++i){
-		plantIconOffsets.push_back(glm::vec2((starting_offset + (50 * i) + (50-maxPlantIconSize)/2)/maxPlantIconSize, (-350 - (50-maxPlantIconSize)/2))/maxPlantIconSize);
+		plantIconOffsets.push_back(glm::vec2((starting_offset + (50 * i) + (50-maxPlantIconSize)/2)/maxPlantIconSize, (-350 - (50-maxPlantIconSize)/2)/maxPlantIconSize));
 		plantIconTexCoords.push_back(ResourceManager::getDepth(player.plants[i].name));
 	}
 }
