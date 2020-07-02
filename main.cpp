77aaaa823fa94f50f327d320d848254b71bb5679
nanoblob-jpeg <jerry.li1248@gmail.com>
2020-07-01 18:41:21 -0700

    fs

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window, int key, int scancode, int action, int mode);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_HEIGHT = 800;
+const unsigned int SCR_WIDTH = 600;
 
 //texture mixing
 float alpha{0.0f};
