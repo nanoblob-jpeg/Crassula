92079acd61437ca125f77bcca047f8f3d3fa1c5b
nanoblob-jpeg <jerry.li1248@gmail.com>
2020-09-24 22:57:59 -0700

    fix plan.h

--- Plant.h
+++ Plant.h
@@ -9,13 +9,14 @@
 #ifndef PLANT
 #define PLANT
 class Plant : public GameObject{
+private:
+	std::vector<std::string> projectileName;
 public:
 	//position for the game is in the 10x10 grid
 	std::string name;
 	std::string textureName;
 	int level;
-
-	std::vector<std::string> projectileName;
+	
 	Plant(){};
 	Plant(std::string pname, std::string ptname, glm::vec2 size, glm::vec3 color = glm::vec3(1.0f));
 
